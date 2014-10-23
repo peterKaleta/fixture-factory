489105ba54129b1a2be50e93ada0fe8445437798
Peter Kaleta <mail@peterkaleta.com>
2014-10-23 18:54:45 +0200

    private methods, default singlevalue generation when no count is provided, generateSingle() got removed

--- index.js
+++ index.js
@@ -7,8 +7,49 @@ function FixtureFactory () {
   this.dataModels = {};
 }
 
-FixtureFactory.prototype = {
+// private methods
+var _generateFixture = function (context, properties) {
 
+  var dataModel = _.isObject(context) ? context : this.dataModels[context] || {};
+  var fixture = _.clone(properties || {});
+  var callStack;
+  var nestedFakerMethod;
+  var nextMethod;
+  var isMethod;
+  var fieldDataModel;
+  _.each(Object.keys(dataModel), function (key) {
+    if (!fixture[key]) {
+      fieldDataModel = dataModel[key];
+
+      if (!_.isObject(fieldDataModel)) {
+        fieldDataModel = {
+          method: fieldDataModel,
+          options: fieldDataModel.options || {}
+        };
+      }
+      callStack = fieldDataModel.method.split('.');
+      nestedFakerMethod = faker;
+      isMethod = true;
+      while (callStack.length) {
+        nextMethod = callStack.shift();
+        if (nestedFakerMethod[nextMethod]) {
+          nestedFakerMethod = nestedFakerMethod[nextMethod];
+        } else {
+          isMethod = false;
+          break;
+        }
+      }
+      fixture[key] = isMethod ?
+                      nestedFakerMethod(fieldDataModel.options) :
+                      fieldDataModel.method;
+
+    }
+
+  });
+  return fixture;
+};
+
+FixtureFactory.prototype = {
   noConflict: function () {
     return new FixtureFactory();
   },
@@ -17,56 +58,11 @@ FixtureFactory.prototype = {
     this.dataModels[key] = dataModel;
   },
 
-  generateSingle: function (context, properties) {
-
-    var dataModel = _.isObject(context) ? context : this.dataModels[context] || {};
-    var fixture = _.clone(properties) || {};
-    var callStack;
-    var nestedFakerMethod;
-    var nextMethod;
-    var isMethod;
-    var fieldDataModel;
-
-    _.each(Object.keys(dataModel), function (key) {
-      if (!fixture[key]) {
-
-        fieldDataModel = dataModel[key];
-
-        if (!_.isObject(fieldDataModel)) {
-          fieldDataModel = {
-            method: fieldDataModel,
-            options: fieldDataModel.options || {}
-          };
-        }
-
-        callStack = fieldDataModel.method.split('.');
-        nestedFakerMethod = faker;
-        isMethod = true;
-        while (callStack.length) {
-          nextMethod = callStack.shift();
-          if (nestedFakerMethod[nextMethod]) {
-            nestedFakerMethod = nestedFakerMethod[nextMethod];
-          } else {
-            isMethod = false;
-            break;
-          }
-        }
-
-        fixture[key] = isMethod ?
-                        nestedFakerMethod(fieldDataModel.options) :
-                        fieldDataModel.method;
-
-      }
-
-    });
-
-    return fixture;
-  },
-
   generate: function (context, count, properties) {
+    count = count || 1;
     var fixtures = [];
     while (fixtures.length < count) {
-      fixtures.push(this.generateSingle(context, properties));
+      fixtures.push(_generateFixture.call(this, context, properties));
     }
     return fixtures;
   }
--- test/fixture-factory.spec.js
+++ test/fixture-factory.spec.js
@@ -18,14 +18,8 @@ var dataModel = {
   someField: 'name.firstName'
 };
 
-fixtureFactory.register('exampleModel', dataModel);
 
 describe('Fixture Factory', function () {
-
-  it('should generate single fixture based on stored dataModel', function () {
-    var fixture = fixtureFactory.generateSingle('exampleModel');
-    expect(fixture.someField).to.exist;
-  });
 
   it('should generate multiple fixtures based on provided dataModel', function () {
     var fixtures = fixtureFactory.generate('exampleModel', 10);
@@ -33,31 +27,30 @@ describe('Fixture Factory', function () {
     fixtures.should.all.have.property('someField');
   });
 
+  it('should default to 1 as a count when not specified for generate multiple', function () {
+    var fixtures = fixtureFactory.generate('exampleModel');
+    expect(fixtures.length).to.equal(1);
+  });
+
   it('should delegate field generation to faker.js', function () {
     var spy = sinon.spy(faker.name, 'firstName');
-    fixtureFactory.generateSingle('exampleModel');
     fixtureFactory.generate('exampleModel', 10);
-    expect(spy).to.have.callCount(11);
+    expect(spy).to.have.callCount(10);
   });
 
   it('should overwrite field with provided values if present', function () {
-    var fixture = fixtureFactory.generateSingle('exampleModel', { someField: 'other value' });
     var fixtures = fixtureFactory.generate('exampleModel', 10, { someField: 'other value' });
-    expect(fixture.someField).to.equal('other value');
     fixtures.should.all.have.property('someField', 'other value');
   });
 
   it('should add field with provided values if not present', function () {
-    var fixture = fixtureFactory.generateSingle('exampleModel', { otherField: 'other value' });
     var fixtures = fixtureFactory.generate('exampleModel', 10, { otherField: 'other value' });
-    expect(fixture.otherField).to.exist;
-    expect(fixture.someField).to.exist;
     fixtures.should.all.have.property('someField');
     fixtures.should.all.have.property('otherField');
   });
 
-  it('in case object is passed as context it shoold be used as model for generation', function () {
-    var fixture = fixtureFactory.generateSingle({ lastName: 'name.lastName' });
+  it('in case object is passed as context it should be used as model for generation', function () {
+    var fixture = fixtureFactory.generate({ lastName: 'name.lastName' })[0];
     expect(fixture.lastName).to.exist;
   });
 
